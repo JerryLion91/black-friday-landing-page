66a59f38c2656d06e01094e87dc810a189e8835a
JerryPC <jeremaios@gmail.com>
2021-07-17 18:52:16 -0300

    styling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 const form = document.getElementById('form');
+const name = document.getElementById('name');
+const option = document.getElementById('option');
+const optionButton = document.getElementById('option-button');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const { value: name } = document.getElementById('name');
   const { value: email } = document.getElementById('email');
-
+  form.style.display = 'none';
+  option.style.display = 'grid';
   addNewClient({
     name,
     email,
   });
+});
+
+optionButton.addEventListener('click', () => {
+  form.style.display = 'grid';
+  option.style.display = 'none';
+});
+
+const arrow = document.getElementById('arrow');
+const footer = document.getElementById('footer');
+
+arrow.addEventListener('click', () => {
+  console.log('object');
+  footer.scrollIntoView({ block: 'end', behavior: 'smooth' });
 });
 
 function addNewClient(newClient) {
